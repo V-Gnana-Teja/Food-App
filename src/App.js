cd9863e29a017848895a3af6b06a4f3871057499
V-Gnana-Teja <151600407+V-Gnana-Teja@users.noreply.github.com>
2024-03-10 16:25:19 +0530

    created menu section, filter section with price and type

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
+import HeadlineCards from "./components/HeadlineCards.jsx";
+import Food from "./components/Food.jsx";
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <HeadlineCards />
+      <Food />
     </div>
   );
 }

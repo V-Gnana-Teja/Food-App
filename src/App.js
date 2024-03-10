73ba2b3520a160d613bf0bf49362ab6bdc8903ce
V-Gnana-Teja <151600407+V-Gnana-Teja@users.noreply.github.com>
2024-03-10 15:09:45 +0530

    created advertisement section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Navbar from './components/Navbar.jsx'
-
+import Hero from './components/Hero.jsx'
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   );
 }
